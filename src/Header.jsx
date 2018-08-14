4eacef8c6e587bdd60c2b5b7059b3023fc0474ec
Andrew Allen <andrew@allenan.com>
2018-08-13 18:59:05 -0700

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <h1 className="App-title">H3Explorer</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
